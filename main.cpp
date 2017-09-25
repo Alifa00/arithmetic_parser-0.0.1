4c6836587e1cc2920b98de6944bb6d6006e3fa17
Vladimir Alefirov <31669425+Alifa00@users.noreply.github.com>
2017-09-25 15:18:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y;
+
+    char com;
+    cin >>x >> com ;
+        if((com=='<')||(com=='>')){
+            cin >> com>>y;
+        }else{cin >>y;}
+
+    if (com=='+'){
+        cout << x+y;
+    }
+    if (com=='-'){
+        cout << x-y;
+    }
+    if (com=='*'){
+        cout << x*y;
+    }
+    if (com=='/'){
+        cout << (double)x/y;
+    }
+    if (com=='%'){
+        cout << x%y;
+    }
+    if (com=='&'){
+        cout << (x & y);
+    }
+    if (com=='|'){
+        cout << ( x|y);
+    }
+    if (com=='^'){
+        cout << (x^y);
+    }
+    if (com=='<'){
+        cout << (x<<y);
+    }
+    if (com=='>'){
+        cout << (x>>y);
+    }
+    return 0;
+}
+
+
